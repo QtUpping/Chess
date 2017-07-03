2158848d3c9bb5a3cdceedb352a137ffb3f63f6c
BenTennyson2017 <708848999@qq.com>
2017-07-03 16:03:11 +0800

    ChessBoard2

--- UserInterface/chessboard.cpp
+++ UserInterface/chessboard.cpp
@@ -4,12 +4,17 @@
 #include <QMouseEvent>
 #include <QDebug>
 
+static MoveRecord *click = new MoveRecord;      //创建一个MoveRecord的类,  通过指针调用choose_or_move函数
+
 ChessBoard::ChessBoard(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::ChessBoard)
 {
     coordinate[0]=oldPoint[0]=coordinate[1]=oldPoint[1]=0;
     ui->setupUi(this);
+//    connect( ui->RCannon,SIGNAL(clicked()), this, SLOT( moveAttempt(int*) ) );  发现信号和槽需要参数类型相同 故这种写法无法使用功能(可以连接) 所以要传坐标参数得去on_pushbutton_clicked中调用函数
+//07-03  可以在ON_BUTTON_CLICK中发送一个带参数的触发信号
+//    connect(ui->RCannon, SIGNAL(MoveChessmanSignal(int *) ), this, SLOT(Capture(int*) ) );
 }
 
 ChessBoard::~ChessBoard()
@@ -19,75 +24,197 @@ ChessBoard::~ChessBoard()
 
 void ChessBoard::mousePressEvent(QMouseEvent *e)
 {
+    int originalXY[2] = {e->x(), e->y()};
     int XY[2]={e->x(), e->y()};
-//    if(validClick(XY))
-         coordinateConversion(XY);
-//    else  qDebug()<<"InvalidClick!";
+    coordinateConversion(XY);
+    click->updateClickCoordinate(originalXY);
 
-    /********显示坐标用**********/
+     qDebug("Movestaty=%d    DBStaty=%d",click->getStatu(),click->getDBStatu() );
+    if( click->getStatu() == false && click->getDBStatu() == false)
+    {
+        qDebug()<<"mouse at "<<originalXY[0]<<","<<originalXY[1];
+        switch(click->getCounter())
+        {
+        case 1:
+            ui->RCannon->move(originalXY[0]-20, originalXY[1]-20);                  //偏移20个像素使按钮处于点击处正中  按钮大小40*40
+            break;
+        case 2:
+            ui->RCannon2->move(originalXY[0]-20, originalXY[1]-20);
+            break;
+        }
+    }
+    click->resetStatuT();
+
+    /********创建坐标按钮**********/
     QPushButton  *btn = new QPushButton(this);
     btn->setText( tr( "(%1,%2)").arg(e->x()).arg(e->y() ) );
     btn->show();
     QPushButton *b2 = new QPushButton(this);
     b2->setText( tr("(%1,%2)").arg(coordinate[0]).arg(coordinate[1] ) );
-    b2->setGeometry(0,50,100,30);
+    b2->move(0,50);
     b2->show();
-
 }
 
-int *ChessBoard::getCurCoordinate(int *point)
+void ChessBoard::Capture(int *XY,int count)
+{
+    if(  click->getStatu() == true  )
+    {
+         click->setCounter(count);
+         qDebug()<<click->getCounter();
+    }
+    else
+    {
+        switch(click->getCounter() )               //根据上一次调用获取的变量值 移动不同棋子
+        {
+        case 1:
+            qDebug()<<"Capture() Move to "<<XY[0]<<","<<XY[1];
+            ui->RCannon->move(XY[0], XY[1] );
+            break;
+        case 2:
+             ui->RCannon2->move(XY[0], XY[1] );
+            break;
+        }
+    }
+}
+
+void ChessBoard::on_RCannon_clicked()
+{
+    int originalXY[2]={ ui->RCannon->x(), ui->RCannon->y() };
+    int XY[2]={ ui->RCannon->x(), ui->RCannon->y() };
+    coordinateConversion(XY);
+
+     /**这两行顺序很重要, 两个函数通过class Movestatu中的MoveStatu的值进行判断并进行修改**/
+    Capture(originalXY,1);
+    click->Choose_or_Move_Judge(1);
+    /**千万不能颠倒, 若颠倒棋子移动功能无法完全实现**/
+
+    /********创建坐标按钮**********/
+    QPushButton *b2 = new QPushButton(this);
+    b2->setText( tr("Cannon1(%1,%2)").arg(coordinate[0]).arg(coordinate[1] ) );
+    b2->setGeometry(0,100,100,30);
+    b2->show();
+}
+void ChessBoard::on_RCannon2_clicked()
+{
+     int originalXY[2] = { ui->RCannon2->x(), ui->RCannon2->y() };
+     int XY[2]={ ui->RCannon2->x(), ui->RCannon2->y() };
+     coordinateConversion(XY);
+     /*---------顺序别乱------------*/
+     Capture(originalXY,2);
+     click->Choose_or_Move_Judge(2);
+    /*-----------------------------*/
+
+     /********创建坐标按钮**********/
+     QPushButton *b2 = new QPushButton(this);
+     b2->setText( tr("Cannon2(%1,%2)").arg(coordinate[0]).arg(coordinate[1] ) );
+     b2->setGeometry(0,150,100,30);
+     b2->show();
+}
+
+
+/*************基本没有问题部分**********************/
+void ChessBoard::coordinateConversion(int *XY)
+{
+    int Xoffset=20, Yoffset=20;
+    int originalPoint[2]={395,250};                 //原点像素坐标
+    int h=50, w=50;                                  //小方格的长宽
+    XY[0]-=originalPoint[0];
+    XY[1]-=originalPoint[1];
+    if(XY[1]>h*4)                                               //考虑楚河宽度 向上偏移  本次资源楚河与方格同宽
+       XY[1]-=0;
+    coordinate[0]=(XY[0]+Xoffset)/w;                     //偏移一定位置使点击交叉点附近区域即可p判断为点击在交叉处
+    coordinate[1]=(XY[1]+Yoffset)/h;
+}
+int *ChessBoard::getConversionCoordinate(int *point)               //需要小改动
 {
      point[0]=coordinate[0];
      point[1]=coordinate[1];
     return point;
 }
+/**********基本没有问题部分**************************/
 
-void ChessBoard::coordinateConversion(int *XY)
+
+/*------------实现功能后移到另一个文件中----------------*/
+MoveRecord::MoveRecord():MoveStatu(true), doubleStatu(true),MovePieceCounter(0)
 {
-    int originalPoint[2]={155,40};                 //原点像素坐标
-    int h=90, w=110;                                  //小方格的长宽
-    XY[0]-=originalPoint[0];
-    XY[1]-=originalPoint[1];
-    if(XY[1]>h*4)                                               //考虑楚河宽度 向上偏移
-       XY[1]-=40;
-    coordinate[0]=(XY[0]+50)/w;                     //偏移一定位置使点击交叉点附近区域即可p判断为点击在交叉处
-    coordinate[1]=(XY[1]+30)/h;
+    Point[0]=Point[1]=-1;
 }
 
-bool ChessBoard::validClick(int *XY)
+void MoveRecord::setCounter(int count)
 {
-     int originalPointEdge[2]={140,20};
-     int opposite[2]={1055,940};
-     for(int i=0; i<2;++i)
-     {
-        if(XY[i]<originalPointEdge[i] && XY[i]>opposite[i] )
-                return false;
-     }
-     return true;
+    MovePieceCounter = count;
 }
 
-void ChessBoard::on_RCannon_clicked()
+int MoveRecord::getCounter()
 {
-   int XY[2]={ ui->RCannon->x(), ui->RCannon->y() };
-    coordinateConversion(XY);
-
-    /********显示坐标用**********/
-    QPushButton *b2 = new QPushButton(this);
-    b2->setText( tr("Cannon1(%1,%2)").arg(coordinate[0]).arg(coordinate[1] ) );
-    b2->setGeometry(0,100,100,30);
-    b2->show();
-    ui->RCannon->move(245,210);
+    return MovePieceCounter;
 }
 
-void ChessBoard::on_RCannon2_clicked()
+void MoveRecord::resetStatuT()
 {
-    int XY[2]={ ui->RCannon2->x(), ui->RCannon2->y() };
-     coordinateConversion(XY);
-
-     /********显示坐标用**********/
-     QPushButton *b2 = new QPushButton(this);
-     b2->setText( tr("Cannon2(%1,%2)").arg(coordinate[0]).arg(coordinate[1] ) );
-     b2->setGeometry(0,150,100,30);
-     b2->show();
-     ui->RCannon2->setGeometry(930,210,41,41);
+    MoveStatu = doubleStatu = true;
 }
+
+bool MoveRecord::getDBStatu()
+{
+    return doubleStatu;
+}
+bool MoveRecord::getStatu()
+{
+    return MoveStatu;
+}
+
+//返回值可以去掉       现在该函数功能为改变棋子的状态
+void MoveRecord::Choose_or_Move_Judge(int count)
+{
+    //-----------------------------测试完毕后这两个分支可合并------------------------------*/
+    if(     MoveStatu == false  && doubleStatu ==false && MovePieceCounter == count   )
+    {
+        qDebug()<<"Cancel movement\n";
+        MoveStatu  =  true;
+    }
+    else if( MoveStatu == false  && doubleStatu ==false && MovePieceCounter != count  )
+    {
+        qDebug()<<"Move to new button pos\n";
+       MoveStatu  =  true;
+    }
+    //--------------------------可合并部分--------------------------------------------------*/
+    else
+    {
+        qDebug()<<"The Button Is Ready To Move! ";
+         MoveStatu = doubleStatu =  false;
+    }
+}
+
+int *MoveRecord::updateClickCoordinate(int *XY)
+{
+    qDebug()<<"Update point"<<XY[0]<<","<<XY[1]<<endl;
+    Point[0]=XY[0];     Point[1]=XY[1];
+    return XY;
+}
+/*--------------------------------------------------------*/
+
+/*--------------------------垃圾分割线---------------------------*/
+//bool ChessBoard::validClick(int *XY)      //暂时不需要
+//{
+//     int originalPointEdge[2]={140,20};
+//     int opposite[2]={1055,940};
+//     for(int i=0; i<2;++i)
+//     {
+//        if(XY[i]<originalPointEdge[i] && XY[i]>opposite[i] )
+//                return false;
+//     }
+//     return true;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
--- UserInterface/chessboard.h
+++ UserInterface/chessboard.h
@@ -14,23 +14,46 @@ class ChessBoard : public QMainWindow
 public:
     explicit ChessBoard(QWidget *parent = 0);
     ~ChessBoard();
-
     void mousePressEvent(QMouseEvent *e);
     //每次鼠标点击后修改coordinate的值(初始为0,0),通过getCurCoordinate提供给外部
-    int *getCurCoordinate(int *point);
-
+    int *getConversionCoordinate(int *point);
+    //修改 每次进行有效点击是将坐标提供给外部
     void coordinateConversion(int *XY);
-    bool validClick(int *XY);                                  //判断点击是否在棋盘内
+
+//    bool validClick(int *XY);         //判断点击是否在棋盘内  暂时无定义
+    void Capture(int *XY, int count);
 
 private slots:
     void on_RCannon_clicked();
-
     void on_RCannon2_clicked();
+
+//signals:
+//    void MoveChessmanSignal(int *XY);
 
 private:
     Ui::ChessBoard *ui;
-    int coordinate[2];              //记录坐标  通过mousePressEvent提供给类外使用
-    int oldPoint[2];                   //前一点坐标
+    int coordinate[2];              //记录坐标  在mousePressEvent更改  getCurCoordinate提供给类外使用
+    int oldPoint[2];                  //前一点坐标  暂时还没用上
+};
+
+/*------若能实现功能则移到另一个文件中--------------*/
+class MoveRecord
+{
+private:
+    int Point[2];
+                                                                    //*************两个bool值均为假时为可移动状态(选中状态),  等待下一次点击时控制按钮移动********************//
+    bool MoveStatu;                                     //记录状态 初始为false 第一次点击(在choose_or_move中使用)改为true 记为选中, 第二次改为false 记为移动 奇数次选中 偶数次移动
+    bool doubleStatu;                                   //只用一个变量监控  会出现可移动状态下按钮可以多次移动的情况  考虑加一个数据  只在点击按钮时改变 doubleStatu初始为真 以控制第一次点击空地不能移动
+    int MovePieceCounter;                            //记录第几个棋子处于可移动状态
+public:
+    MoveRecord();
+    void setCounter(int count);
+    int getCounter();
+    void resetStatuT();                                       //用于在鼠标点击事件中将DBS设为真
+    bool getDBStatu();
+    bool getStatu();                                          //提供MoveStatu状态的接口
+    void Choose_or_Move_Judge(int count);    //改变状态函数, 若判断可移动则更新为下一状态
+    int *updateClickCoordinate(int *XY);        //更新坐标point   然而point的坐标没有用上 看起来与coordinate功能重复
 };
 
 #endif // CHESSBOARD_H
